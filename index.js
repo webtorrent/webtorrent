b0613e52b9e424b56521f6f5f8749d55297f6d3b
Feross Aboukhadijeh <feross@feross.org>
2013-11-09 18:39:15 -0800

    dht: use node-style dgram API

--- index.js
+++ index.js
@@ -1,35 +1,50 @@
+/* global chrome */
+
+// TODO: get .torrent file from magnet link
+
 // window.log = function (data) {
 //   document.getElementById('console').innerHTML += data + '<br>'
 // }
-window.log = function (/* ... */) {
-  if (process.env.DEBUG !== 'false') console.log.apply(console, arguments)
-}
-window.bops = require('bops')
-
-var isChromeApp = !!(window.chrome && chrome.app && chrome.app.runtime)
-if (isChromeApp) {
-  log('This is a Chrome App')
-}
-
-var magnet = require('magnet-uri')
+// window.log = function (/* ... */) {
+//   if (process.env.DEBUG !== 'false') console.log.apply(console, arguments)
+// }
 
 var DHT = require('./lib/bittorrent-dht')
-var pride = '1E69917FBAA2C767BCA463A96B5572785C6D8A12'
-var leaves = 'D2474E86C95B19B8BCFDB92BC12C9D44667CFA36'
+var hat = require('hat')
+var magnet = require('magnet-uri')
+var Swarm = require('./lib/peer-wire-swarm')
+
+window.bops = require('bops')
+var isChromeApp = !!(window.chrome && chrome.app && chrome.app.runtime)
+if (isChromeApp) {
+  console.log('This is a Chrome App')
+}
+
+var peerId = '-WW0001-'+hat(48)
+
+// var pride = '1E69917FBAA2C767BCA463A96B5572785C6D8A12'
+// var leaves = 'D2474E86C95B19B8BCFDB92BC12C9D44667CFA36'
 
 var leavesMagnet = 'magnet:?xt=urn:btih:d2474e86c95b19b8bcfdb92bc12c9d44667cfa36&dn=Leaves+of+Grass+by+Walt+Whitman.epub&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80&tr=udp%3A%2F%2Ftracker.istole.it%3A6969&tr=udp%3A%2F%2Ftracker.ccc.de%3A80&tr=udp%3A%2F%2Fopen.demonii.com%3A1337'
 
-var parse = magnet(leavesMagnet)
-var infoHash = parse.xt.split('urn:btih:')[1]
+var infoHash
+try {
+  infoHash = magnet(leavesMagnet).xt.split('urn:btih:')[1]
+} catch (e) {}
 
-window.dht = new DHT(infoHash)
+
+var dht = window.dht = new DHT(infoHash)
+var swarm = new Swarm(infoHash, peerId)
+
 dht.on('node', function (node, infoHash) {
   // log('node: ' + node)
 })
 dht.on('peer', function (peer, infoHash) {
-  log('peer: ' + peer)
+  swarm.add(peer)
 })
 dht.findPeers(300)
+
+var METADATA_BLOCK_SIZE = 1024*16
 
 
 // var compact2string = require("compact2string");
@@ -60,40 +75,3 @@ dht.findPeers(300)
 // console.log(original)
 
 
-
-// Send UDP packet to echo server
-// var string = require('./lib/string')
-// var socket = require('./socket')
-
-// var sock = new socket.UDPSocket()
-// sock.on('bound', function(port) {
-//   console.log('Bound to port: ' + port)
-// })
-
-// sock.on('data', function(data, host, port) {
-//   console.log('Got data from host ' + host + ' port ' + port + ': ' + string.fromUTF8Arr(data))
-// })
-
-// sock.sendTo('lol', 'localhost', 50963)
-
-
-
-// // Send TCP to echo server
-// var string = require('./lib/string')
-// var socket = require('./socket')
-
-// var listenSock = new socket.TCPListenSocket(1701)
-// listenSock.on('connected', function(connSock) {
-//   console.log('Connection opened from ' + connSock.host + ':' + connSock.port)
-//   connSock.write('You are connected!')
-//   connSock.on('data', function (data) {
-//   	console.log('Got data: ' + bops.to(data))
-//   })
-// })
-
-// var clientSock = new socket.TCPSocket('127.0.0.1', 1702)
-// clientSock.on('data', function (data) {
-// 	console.log('Got data: ' + bops.to(data))
-// })
-
-// clientSock.write('hello, world!')
