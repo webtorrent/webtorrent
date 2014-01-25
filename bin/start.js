3b058bc360de477a90a5f3b1442e1814a9de5c0e
Feross Aboukhadijeh <feross@feross.org>
2014-01-25 00:01:10 -0800

    add super useful `npm run watch` command!

---
+++ bin/start.js
@@ -0,0 +1,12 @@
+var cp = require('child_process')
+
+var BIN = '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
+var ARGS = ['--load-and-launch-app=chrome']
+
+var child = cp.spawn(BIN, ARGS)
+
+// Nodemon is trying to kill us, so kill Chrome
+process.once('SIGUSR2', function () {
+  child.kill()
+  process.kill(process.pid, 'SIGUSR2')
+})
--- chrome/background.js
+++ chrome/background.js
@@ -1,8 +1,8 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('window.html', {
-    'bounds': {
-      'width': 400,
-      'height': 500
+    bounds: {
+      width: 400,
+      height: 500
     }
   });
 });
