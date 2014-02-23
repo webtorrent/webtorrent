72fbf37d0510ac5e8fe34cb6eff6508cea54109c
Feross Aboukhadijeh <feross@feross.org>
2014-02-22 16:21:12 -0800

    add todos

--- index.js
+++ index.js
@@ -12,8 +12,22 @@ if (window.name === 'app') {
   new TorrentManager()
 }
 
+// "file_handlers": {
+//   "torrent": {
+//     "types": [
+//       "application/x-bittorrent"
+//     ],
+//     "title": "WebTorrent (BitTorrent client)"
+//   }
+// },
 
+// navigator.registerProtocolHandler('web+mystuff', 'http://example.com/rph?q=%s', 'My App')
 
+// TODO: if ESC and fullscreen, exit fullscreen
 
+// chrome.runtime.onSuspend.addListener(function() {
+//   // Do some simple clean-up tasks.
+// })
 
-
+// TODO: online/offline status
+// https://developer.mozilla.org/en-US/docs/Online_and_offline_events
