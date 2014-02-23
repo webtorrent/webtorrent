c5019d1200a8eccb30a58f7566f3ea003c47674c
Feross Aboukhadijeh <feross@feross.org>
2014-02-23 15:37:30 -0800

    hook up ui! (eliminate handlebars - it didn't help enough)

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-// Load pre-compiled handlebars templates and put them on require('handlebars').templates
-if (!require('handlebars').templates) {
-  require('./views/compiled')
-}
-
 var App = require('./lib/app')
 var TorrentManager = require('./lib/torrent-manager')
 
