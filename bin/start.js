771eb189312ea578ff2b4763dc86c197b8906f74
Feross Aboukhadijeh <feross@feross.org>
2014-03-16 18:59:33 -0700

    code style

--- bin/start.js
+++ bin/start.js
@@ -2,13 +2,9 @@
 
 var cp = require('child_process')
 
-var BIN = process.platform=='linux' ? '/bin/google-chrome' : '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary';
+var BIN = process.platform === 'linux'
+  ? '/bin/google-chrome'
+  : '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
 var ARGS = ['--load-and-launch-app=chrome']
 
 var child = cp.spawn(BIN, ARGS)
-
-// Nodemon is trying to kill us, so kill Chrome
-process.once('SIGUSR2', function () {
-  child.kill()
-  process.kill(process.pid, 'SIGUSR2')
-})
