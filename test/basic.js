07c5f2008eb3540e7ab4525f9ae06d33f167904d
Feross Aboukhadijeh <feross@feross.org>
2014-06-04 19:00:16 -0700

    add basic sanity check tests

--- test/basic.js
+++ test/basic.js
@@ -1,6 +1,25 @@
+var cp = require('child_process')
 var test = require('tape')
+var WebTorrent = require('../')
 
-test('TODO', function (t) {
-  t.ok(true)
-  t.end()
+/**
+ * Extensive bittorrent functionality tests are contained within dependencies like
+ * `bittorrent-client`, `bitorrent-protocol`, etc.
+ */
+
+test('Module usage (sanity check)', function (t) {
+  var client = new WebTorrent()
+  t.equal(typeof client.add, 'function', 'client.add exists')
+  client.destroy(function () {
+    t.pass('client.destroy works')
+    t.end()
+  })
+})
+
+test('Command line usage (sanity check)', function (t) {
+  var bin = __dirname + '/../bin/cmd.js'
+  cp.exec(bin, function (err, code) {
+    t.error(err) // no error, exit code 0
+    t.end()
+  })
 })
