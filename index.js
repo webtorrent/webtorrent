6cab794d4df6f3aa6d30d419b3acff6e9ce89f73
Feross Aboukhadijeh <feross@feross.org>
2014-12-16 18:09:30 -0800

    delete comments; don't repeat readme

--- index.js
+++ index.js
@@ -121,15 +121,6 @@ WebTorrent.prototype.get = function (torrentId) {
 
 /**
  * Start downloading a new torrent. Aliased as `client.download`.
- *
- * `torrentId` can be one of:
- *   - magnet uri (utf8 string)
- *   - torrent file (buffer)
- *   - info hash (hex string or buffer)
- *   - parsed torrent (from [parse-torrent](https://github.com/feross/parse-torrent))
- *   - http/https url to a torrent file (string)
- *   - filesystem path to a torrent file (string)
- *
  * @param {string|Buffer|Object} torrentId
  * @param {Object} opts torrent-specific options
  * @param {function=} ontorrent called when the torrent is ready (has metadata)
@@ -179,13 +170,6 @@ WebTorrent.prototype.download = function (torrentId, opts, ontorrent) {
 
 /**
  * Start seeding a new torrent.
- *
- * `input` can be any of the following:
- *   - path to the file or folder on filesystem (string)
- *   - W3C File object (from an `<input>` or drag and drop)
- *   - W3C FileList object (basically an array of `File` objects)
- *   - Array of `File` objects
- *
  * @param  {string|File|FileList|Buffer|Array.<File|Buffer>} input
  * @param  {Object} opts
  * @param  {function} onseed
@@ -226,7 +210,6 @@ WebTorrent.prototype.seed = function (input, opts, onseed) {
 
 /**
  * Remove a torrent from the client.
- *
  * @param  {string|Buffer}   torrentId
  * @param  {function} cb
  */
@@ -241,8 +224,6 @@ WebTorrent.prototype.remove = function (torrentId, cb) {
 
 /**
  * Destroy the client, including all torrents and connections to peers.
- *
- * @override
  * @param  {function} cb
  */
 WebTorrent.prototype.destroy = function (cb) {
@@ -263,7 +244,7 @@ WebTorrent.prototype.destroy = function (cb) {
 }
 
 /**
- * Check if `obj` is a W3C Blob object (which is the superclass of W3C File)
+ * Check if `obj` is a W3C Blob object (which is the superclass of W3C File).
  * @param  {*} obj
  * @return {boolean}
  */
