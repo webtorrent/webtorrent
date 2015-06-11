cc20e47f27798028c533965ea160a3b42e6999ca
Feross Aboukhadijeh <feross@feross.org>
2015-06-10 21:00:42 -0700

    add failing test for #348

---
+++ test/duplicates.js
@@ -0,0 +1,48 @@
+var fs = require('fs')
+var test = require('tape')
+var WebTorrent = require('../')
+
+var leavesBook = fs.readFileSync(__dirname + '/content/Leaves of Grass by Walt Whitman.epub')
+
+test('client.seed followed by duplicate client.add', function (t) {
+  t.plan(3)
+
+  var opts = {
+    name: 'Leaves of Grass by Walt Whitman.epub'
+  }
+
+  var client = new WebTorrent({ dht: false, tracker: false })
+  client.seed(leavesBook, opts, function (torrent1) {
+    client.add(torrent1.infoHash, function (torrent2) {
+      t.equal(torrent1.infoHash, torrent2.infoHash)
+      t.equal(client.torrents.length, 1)
+      client.destroy(function () {
+        t.pass('destroyed client')
+      })
+    })
+  })
+})
+
+test('client.seed followed by duplicate client.add, twice', function (t) {
+  t.plan(5)
+
+  var opts = {
+    name: 'Leaves of Grass by Walt Whitman.epub'
+  }
+
+  var client = new WebTorrent({ dht: false, tracker: false })
+
+  client.seed(leavesBook, opts, function (torrent1) {
+    client.add(torrent1.infoHash, function (torrent2) {
+      t.equal(torrent1.infoHash, torrent2.infoHash)
+      t.equal(client.torrents.length, 1)
+      client.add(torrent1.infoHash, function (torrent2) {
+        t.equal(torrent1.infoHash, torrent2.infoHash)
+        t.equal(client.torrents.length, 1)
+        client.destroy(function () {
+          t.pass('destroyed client')
+        })
+      })
+    })
+  })
+})
