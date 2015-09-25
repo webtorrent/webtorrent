3a1abc5d60e089cebc35cec054898d86bc281072
Eric Wooley <ericwooley@gmail.com>
2015-09-24 19:46:55 -0700

    Documented, renamed, and refactored long methods

--- index.js
+++ index.js
@@ -230,7 +230,7 @@ WebTorrent.prototype.seed = function (input, opts, onseed) {
         _onseed()
         return
       } else {
-        torrent._onTorrentId(torrentBuf)
+        torrent._createParsedTorrent(torrentBuf)
       }
     })
   })
