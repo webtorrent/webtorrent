38824d693765ed00bd02d05b35a10659fb636883
Joseph Frazier <1212jtraceur@gmail.com>
2016-02-08 22:06:45 -0500

    Tests ensure Travis environment for browser tests

--- bin/test.js
+++ bin/test.js
@@ -2,8 +2,8 @@
 
 var spawn = require('cross-spawn-async')
 
-var runBrowserTests = !process.env.TRAVIS_PULL_REQUEST ||
-  process.env.TRAVIS_PULL_REQUEST === 'false'
+var runBrowserTests = process.env.TRAVIS && (!process.env.TRAVIS_PULL_REQUEST ||
+  process.env.TRAVIS_PULL_REQUEST === 'false')
 
 var node = spawn('npm', ['run', 'test-node'], { stdio: 'inherit' })
 node.on('close', function (code) {
