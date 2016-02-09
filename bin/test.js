d5aa948538671cb0c445ed8c5de71469e99c11c7
Joseph Frazier <1212jtraceur@gmail.com>
2016-02-08 22:08:23 -0500

    npm test uses Electron if saucelabs cannot be used

--- bin/test.js
+++ bin/test.js
@@ -5,14 +5,17 @@ var spawn = require('cross-spawn-async')
 var runBrowserTests = process.env.TRAVIS && (!process.env.TRAVIS_PULL_REQUEST ||
   process.env.TRAVIS_PULL_REQUEST === 'false')
 
-var node = spawn('npm', ['run', 'test-node'], { stdio: 'inherit' })
-node.on('close', function (code) {
-  if (code === 0 && runBrowserTests) {
-    var browser = spawn('npm', ['run', 'test-browser'], { stdio: 'inherit' })
-    browser.on('close', function (code) {
+npmRun('test-node', function (code) {
+  if (code === 0) {
+    var scriptName = runBrowserTests ? 'test-browser' : 'test-browser-headless'
+    npmRun(scriptName, function (code) {
       process.exit(code)
     })
   } else {
     process.exit(code)
   }
 })
+
+function npmRun (scriptName, onClose) {
+  spawn('npm', ['run', scriptName], { stdio: 'inherit' }).on('close', onClose)
+}
