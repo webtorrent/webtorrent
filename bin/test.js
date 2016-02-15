7e61cfd5520044b840a81511a4fa4518b9467885
Joseph Frazier <1212jtraceur@gmail.com>
2016-02-14 20:53:36 -0500

    Use .zuulrc credentials for Sauce Labs tests
    See https://github.com/feross/webtorrent/pull/605#issuecomment-183823968

--- bin/test.js
+++ bin/test.js
@@ -1,8 +1,15 @@
 #!/usr/bin/env node
 
 var spawn = require('cross-spawn-async')
+var findNearestFile = require('find-nearest-file')
+var path = require('path')
+var userHome = require('user-home')
+var pathExists = require('path-exists')
 
-var runSauceLabs = process.env.SAUCE_USERNAME && process.env.SAUCE_ACCESS_KEY
+// .zuulrc logic from https://github.com/defunctzombie/zuul/blob/a0de46a5906c84b19f655c487f7c8debe938984d/bin/zuul#L384
+var zuulrcPath = findNearestFile('.zuulrc') || path.join(userHome, '.zuulrc')
+var hasSauceLabEnvVars = process.env.SAUCE_USERNAME && process.env.SAUCE_ACCESS_KEY
+var runSauceLabs = hasSauceLabEnvVars || pathExists.sync(zuulrcPath)
 
 npmRun('test-node', function (code) {
   if (code === 0) {
