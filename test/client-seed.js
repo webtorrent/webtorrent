d98414904816b93539b604f695a0c7103c9263b3
Romain Beaumont <romain.rom1@gmail.com>
2016-03-14 11:45:04 +0100

    fix tests : remove the trackers when testing the magnetURI in the browser

--- test/client-seed.js
+++ test/client-seed.js
@@ -17,7 +17,7 @@ test('client.seed: torrent file (Buffer)', function (t) {
   }, function (torrent) {
     t.equal(client.torrents.length, 1)
     t.equal(torrent.infoHash, fixtures.leaves.parsedTorrent.infoHash)
-    t.equal(torrent.magnetURI, fixtures.leaves.magnetURI)
+    t.equal(torrent.magnetURI.split("&tr")[0], fixtures.leaves.magnetURI)
 
     client.remove(torrent, function (err) { t.error(err, 'torrent removed') })
     t.equal(client.torrents.length, 0)
@@ -40,7 +40,7 @@ test('client.seed: torrent file (Buffer), set name on buffer', function (t) {
   client.seed(buf, function (torrent) {
     t.equal(client.torrents.length, 1)
     t.equal(torrent.infoHash, fixtures.leaves.parsedTorrent.infoHash)
-    t.equal(torrent.magnetURI, fixtures.leaves.magnetURI)
+    t.equal(torrent.magnetURI.split("&tr")[0], fixtures.leaves.magnetURI)
 
     client.remove(torrent, function (err) { t.error(err, 'torrent removed') })
     t.equal(client.torrents.length, 0)
@@ -64,7 +64,7 @@ test('client.seed: torrent file (Blob)', function (t) {
   }, function (torrent) {
     t.equal(client.torrents.length, 1)
     t.equal(torrent.infoHash, fixtures.leaves.parsedTorrent.infoHash)
-    t.equal(torrent.magnetURI, fixtures.leaves.magnetURI)
+    t.equal(torrent.magnetURI.split("&tr")[0], fixtures.leaves.magnetURI)
 
     client.remove(torrent, function (err) { t.error(err, 'torrent removed') })
     t.equal(client.torrents.length, 0)
