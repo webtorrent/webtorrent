89a614fb09ade114c446faf7aee820188e36695d
ZunSThy <zunsthy@gmail.com>
2016-11-06 15:47:52 +0800

    Revert "add 'use-agent' option"
    This reverts commit edcb237a03c83d2837813abef2fcd62295e052a0.

--- index.js
+++ index.js
@@ -78,12 +78,6 @@ function WebTorrent (opts) {
   }
   self.nodeIdBuffer = Buffer.from(self.nodeId, 'hex')
 
-  if (typeof opts.userAgent === 'string') {
-    self.userAgent = opts.userAgent
-  } else {
-    self.userAgent = 'WebTorrent/' + VERSION + ' (https://webtorrent.io)'
-  }
-
   self.destroyed = false
   self.listening = false
   self.torrentPort = opts.torrentPort || 0
