df6e2055a3a4eafa18220e78f41e4d3b3e27694c
Kelvin Lu <kelvinlu@mit.edu>
2018-05-11 20:24:08 -0400

    PE/MSE Implementation for WebTorrent

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ var parseTorrent = require('parse-torrent')
 var path = require('path')
 var Peer = require('simple-peer')
 var randombytes = require('randombytes')
+var sha1 = require('simple-sha1')
 var speedometer = require('speedometer')
 var zeroFill = require('zero-fill')
 
@@ -457,6 +458,21 @@ WebTorrent.prototype._debug = function () {
   var args = [].slice.call(arguments)
   args[0] = '[' + this._debugId + '] ' + args[0]
   debug.apply(null, args)
+}
+
+WebTorrent.prototype._getByHash = function (infoHashHash) {
+  var i, torrent
+  var len = this.torrents.length
+  var hReq2 = Buffer.from('req2', 'utf8').toString('hex')
+
+  for (i = 0; i < len; i++) {
+    torrent = this.torrents[i]
+    if (infoHashHash === sha1.sync(Buffer.from(hReq2 + torrent.infoHash, 'hex'))) {
+      return torrent
+    }
+  }
+
+  return null
 }
 
 /**
