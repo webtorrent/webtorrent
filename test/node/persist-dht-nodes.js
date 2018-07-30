c2f594385e7c0f27a4a373ebdd1adf632819cb58
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-07-29 23:33:43 -0400

    Add save DHT state test

---
+++ test/node/persist-dht-nodes.js
@@ -0,0 +1,41 @@
+var test = require('tape')
+var tmp = require('tmp')
+var fs = require('fs')
+var DHT = require('bittorrent-dht/server')
+var WebTorrent = require('../../')
+
+var localHost = '127.0.0.1'
+var port = 9999
+
+test('Save DHT state', function (t) {
+  t.plan(4)
+  var saveFile = tmp.tmpNameSync()
+  var dhtServer = new DHT({ bootstrap: false })
+  dhtServer.on('error', function (err) { t.fail(err) })
+  dhtServer.on('warning', function (err) { t.fail(err) })
+  dhtServer.listen(port, function handleServerListening () {
+    var client = new WebTorrent({
+      dht: { bootstrap: false },
+      dhtState: saveFile
+    })
+    client.on('error', function (err) { t.fail(err) })
+    client.on('warning', function (err) { t.fail(err) })
+    client.dht.addNode({ host: localHost, port: port })
+    client.dht.on('node', function handleNodeAdded () {
+      client.saveDhtState(function handleDhtStateSaved () {
+        var dhtStateJson = fs.readFileSync(saveFile)
+        var dhtState = JSON.parse(dhtStateJson)
+        var nodes = dhtState.nodes
+        var node = nodes[0]
+        t.equal(node.host, localHost)
+        t.equal(node.port, port)
+        client.destroy(function handleClientDestroyed (err) {
+          t.error(err, 'client destroyed')
+        })
+        dhtServer.destroy(function handleDhtServerDestroyed (err) {
+          t.error(err, 'dht server destroyed')
+        })
+      })
+    })
+  })
+})
