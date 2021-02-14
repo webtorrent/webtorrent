d009f3fba0dda691dcf63838350569e53472a451
Nas Kavian <>
2021-02-14 13:44:28 -0800

    Support adding paused torrents.

--- test/client-add.js
+++ test/client-add.js
@@ -205,3 +205,24 @@ test('client.add: invalid torrent id: short buffer', function (t) {
 
   client.add(Buffer.from('abc'))
 })
+
+test('client.add: paused torrent', function (t) {
+  t.plan(5)
+
+  const client = new WebTorrent({ dht: false, tracker: false, lsd: false })
+
+  client.on('error', function (err) { t.fail(err) })
+  client.on('warning', function (err) { t.fail(err) })
+
+  const torrent = client.add(fixtures.leaves.magnetURI, { paused: true })
+  t.equal(client.torrents.length, 1)
+
+  torrent.on('infoHash', function () {
+    t.equal(torrent.paused, true)
+
+    client.remove(fixtures.leaves.magnetURI, function (err) { t.error(err, 'torrent destroyed') })
+    t.equal(client.torrents.length, 0)
+
+    client.destroy(function (err) { t.error(err, 'client destroyed') })
+  })
+})
