87c7d2d4a2a565f36dfadbb04f9f5cbc450d26ec
R0quef0rt <ryanjbrooks11@gmail.com>
2021-03-17 05:58:52 -0500

    Simplify function

--- hacks.js
+++ hacks.js
@@ -3,25 +3,15 @@
 // This error leads to WebTorrent Desktop and Chromium-based browsers being completely
 // unable to seed until the process is restarted:
 // https://github.com/webtorrent/webtorrent/issues/1981
-let gcInterval = 1
 function forcePeerGC () {
-  let peer = new RTCPeerConnection()
-  setTimeout(() => {
-    peer.close()
-    peer = null
-  }, 10)
-  console.log(gcInterval++)
-  if (!(gcInterval % 20)) {
-    queueMicrotask(() => {
-      console.log('Collecting garbage.')
-      let img = document.createElement('img')
-      img.src = window.URL.createObjectURL(new Blob([new ArrayBuffer(5e+7)]))
-      img.onerror = function () {
-        window.URL.revokeObjectURL(this.src)
-        img = null
-      }
-    })
-  }
+  queueMicrotask(() => {
+    let img = document.createElement('img')
+    img.src = window.URL.createObjectURL(new Blob([new ArrayBuffer(5e+7)]))
+    img.onerror = function () {
+      window.URL.revokeObjectURL(this.src)
+      img = null
+    }
+  })
 }
 
-setInterval(forcePeerGC, 5000)
+setInterval(forcePeerGC, 30000)
