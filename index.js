3f92ddf5bd190fba79c0af5324b2326ffeebf7cd
Bruce-Hopkins-Jr <behopkinsjr@gmail.com>
2022-01-09 12:41:10 +0100

    refactor: passes duplicate torrent in cb when creating seed

--- index.js
+++ index.js
@@ -393,7 +393,10 @@ class WebTorrent extends EventEmitter {
 
           const existingTorrent = this.get(torrentBuf)
           if (existingTorrent) {
-            torrent._destroy(new Error(`Cannot add duplicate torrent ${existingTorrent.infoHash}`))
+            console.warn("A torrent with the same seed is already in the client.")
+            torrent._destroy();
+            if (typeof onseed === 'function') onseed(existingTorrent, {isDup: true})
+
           } else {
             torrent._onTorrentId(torrentBuf)
           }
