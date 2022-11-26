dea658a7b50a9203d1113a2459d6c99dd456b895
Gary Borton <gdborton@gmail.com>
2022-11-26 02:53:49 -0800

    Fix chrome stream, add test for small file.

--- test/file-stream.js
+++ test/file-stream.js
@@ -1,9 +1,9 @@
 const test = require('tape')
 const WebTorrent = require('..')
 
-test('file-stream fetches chunks in parallel', (t) => {
+test('file-stream fetches chunks in parallel, works for large files', (t) => {
   t.plan(2)
-  const client = new WebTorrent({ dht: false, announce: [] })
+  const client = new WebTorrent({ dht: false, tracker: false, lsd: false })
   const startingData = Buffer.from('0'.repeat(1024 * 1024 * 100))
   client.seed(startingData, {
     announce: []
@@ -25,13 +25,42 @@ test('file-stream fetches chunks in parallel', (t) => {
     })
     readStream.on('end', () => {
       const readTime = Date.now() - start
+      t.deepEqual(Buffer.concat(data), startingData)
       // reading should be much faster than doing so sequentially,
       // but it's difficult to assert this based solely on clock.
       // Let's just shoot for 20x faster.
       const upperLimit = artificialDelay * torrent.pieces.length / 20
       t.assert(readTime < upperLimit, `expect read time: ${readTime} to be less than ${upperLimit}`)
-      t.deepEqual(Buffer.concat(data), startingData)
       client.destroy()
     })
   })
 })
+
+test('file-stream works for small files', (t) => {
+  t.plan(1)
+  const client = new WebTorrent({ dht: false, tracker: false, lsd: false })
+  const startingData = Buffer.from('0'.repeat(1023 * 100))
+  client.seed(startingData, {
+    dht: false,
+    announce: []
+  }, torrent => {
+    const artificialDelay = 10 // simulate a slow chunk look up, 10ms
+    const origGet = torrent.store.get
+    torrent.store.get = function (...rest) {
+      const _this = this
+      setTimeout(() => {
+        origGet.apply(_this, rest)
+      }, artificialDelay)
+    }
+    // time the total
+    const readStream = torrent.files[0].createReadStream()
+    const data = []
+    readStream.on('data', (d) => {
+      data.push(d)
+    })
+    readStream.on('end', () => {
+      t.deepEqual(Buffer.concat(data), startingData)
+      client.destroy()
+    })
+  });
+});
