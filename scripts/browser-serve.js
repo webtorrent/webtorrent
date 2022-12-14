9ac1dfacb25af85bfe2360fdc1872e41034e9efb
Cas <6506529+ThaUnknown@users.noreply.github.com>
2022-12-14 18:01:17 +0100

    perf: drop rusha, drop Buffer (#2390)

--- scripts/browser-serve.js
+++ scripts/browser-serve.js
@@ -3,5 +3,5 @@ import finalhandler from 'finalhandler'
 import { createServer } from 'http'
 
 createServer((req, res) => {
-  serve('./')(req, res, finalhandler(req, res))
+  serve('./dist/')(req, res, finalhandler(req, res))
 }).listen(process.env.AIRTAP_SUPPORT_PORT)
