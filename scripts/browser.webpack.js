948d0683a2eafd2805a11aa5e76d85d8117af59b
RohanHandore <110839432+RohanHandore@users.noreply.github.com>
2023-04-24 12:35:20 +0530

    Improvements to a Webpack Configuration File
    Changed require statements to import statements:
    
    Title: Use ES6 import statements
    Used single quotes for string literals consistently:
    
    Title: Use consistent string quotes
    Used object shorthand syntax when possible:
    
    Title: Use object shorthand syntax
    Used arrow function expressions instead of function declarations:
    
    Title: Use arrow function expressions for optimization configuration

--- scripts/browser.webpack.js
+++ scripts/browser.webpack.js
@@ -1,8 +1,7 @@
-import webpack from 'webpack'
-import TerserPlugin from 'terser-webpack-plugin'
-import info from '../package.json' assert { type: 'json' }
+import webpack from 'webpack';
+import TerserPlugin from 'terser-webpack-plugin';
+import info from '../package.json' assert { type: 'json' };
 
-/** @type {import('webpack').WebpackOptionsNormalized} */
 export default {
   entry: './index.js',
   devtool: 'source-map',
@@ -12,39 +11,41 @@ export default {
       ...info.browser,
       crypto: false,
       stream: 'readable-stream',
-      path: 'path-browserify'
-    }
+      path: 'path-browserify',
+    },
   },
   output: {
     chunkFormat: 'module',
     filename: 'webtorrent.min.js',
     library: {
-      type: 'module'
-    }
+      type: 'module',
+    },
   },
   mode: 'production',
   target: 'web',
   experiments: {
-    outputModule: true
+    outputModule: true,
   },
   plugins: [
     new webpack.ProvidePlugin({
       process: '/polyfills/process-fast.js',
-      Buffer: ['buffer', 'Buffer']
+      Buffer: ['buffer', 'Buffer'],
     }),
     new webpack.DefinePlugin({
-      global: 'globalThis'
-    })
+      global: 'globalThis',
+    }),
   ],
   optimization: {
     minimize: true,
-    minimizer: [new TerserPlugin({
-      terserOptions: {
-        format: {
-          comments: false
-        }
-      },
-      extractComments: false
-    })]
-  }
-}
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
+    ],
+  },
+};
