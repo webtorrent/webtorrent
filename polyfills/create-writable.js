881b4e4fda96977119955ffd6fd8570d88d7cc83
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2024-06-29 12:32:39 +0200

    chore: document polyfill

--- polyfills/create-writable.js
+++ polyfills/create-writable.js
@@ -1,2 +1,4 @@
 // fix for createWritable() not being available in Safari
+// https://caniuse.com/mdn-api_filesystemfilehandle_createwritable
+// https://bugs.webkit.org/show_bug.cgi?id=254726
 import 'native-file-system-adapter/src/FileSystemFileHandle.js'
