04ced365f93350682d548c787866af7b0b1f698f
Brad Marsden <silentbot1@gmail.com>
2024-06-29 21:35:11 +0100

    fix: revert "fix: drop IDB chunk store (#2553)"
    This reverts commit 18a468304c3826da42d6e4a4bc7e074ea9297187.

--- polyfills/create-writable.js
+++
@@ -1,4 +0,0 @@
-// fix for createWritable() not being available in Safari
-// https://caniuse.com/mdn-api_filesystemfilehandle_createwritable
-// https://bugs.webkit.org/show_bug.cgi?id=254726
-import 'native-file-system-adapter/src/FileSystemFileHandle.js'
