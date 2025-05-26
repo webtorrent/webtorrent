bfd878e3dcf000b036af64f71c0c947e2fc85ba8
parsa <parsajavanshir@gmail.com>
2025-05-27 00:51:21 +0330

    fix: move serve-static middleware out of request handler

--- scripts/browser-serve.js
+++ scripts/browser-serve.js
@@ -2,6 +2,9 @@ import serve from 'serve-static'
 import finalhandler from 'finalhandler'
 import { createServer } from 'http'
 
+// instantiate middleware only once (outside request handler)
+const serveStatic = serve('./dist/')
+
 createServer((req, res) => {
-  serve('./dist/')(req, res, finalhandler(req, res))
+  serveStatic(req, res, finalhandler(req, res))
 }).listen(process.env.AIRTAP_SUPPORT_PORT)
